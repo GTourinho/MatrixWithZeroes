3d7ab8ad9dd9c7c1ce31c6b2c994e372590713c5
Gabriel Tourinho <>
2021-04-14 17:31:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+# Feito por: Gabriel Tourinho Badaró
+# Teste Avaliativo - Processo LexisNexis
+# Escreva um algoritmo para que se um elemento em uma matriz MxN é 0, todos os elementos da linha e coluna deste elemento recebam também o valor 0.
+# Write an algorithm so that if an element in an MxN matrix is 0, all the elements of the row and column of this element are also given the value 0
+
+#Helper functions
+def getmatrix():
+  matrix = []
+  nRows = 1
+  row = [int(n) for n in input().split()]
+  nColumns = len(row)
+  matrix.append(row)
+  while nColumns > 0:
+    row = [int(n) for n in input().split()]
+    if row == []:
+      break
+    if len(row) != nColumns:
+      exit("Error: different row lengths")
+    matrix.append(row)
+    nRows+=1
+  return matrix, nRows, nColumns
+
+#Get Matrix
+print('Insert matrix lines (blank line to end):')
+matrix, nRows, nColumns = getmatrix()
+if matrix == [[]]:
+  exit('Error: empty matrix')
+
+#Helper hashes
+intactRows = {}
+foundZeroColumns = {}
+
+#Draw Rows, find Columns with zeroes and Intact Rows
+for row in range(nRows):
+  draw = False
+  for column in range(nColumns):
+    if matrix[row][column] == 0:
+      draw = True
+      foundZeroColumns[column] = 1
+  if draw:
+    matrix[row] = [0]*nColumns
+  else:
+    intactRows [row] = 1
+
+#Draw Columns
+for column in foundZeroColumns:
+  for row in intactRows:
+    matrix[row][column] = 0
+
+#Print result
+for row in matrix:
+  for column in range(nColumns):
+    print(row[column], end = '')
+    print(' ', end = '')
+  print('')
+
+
